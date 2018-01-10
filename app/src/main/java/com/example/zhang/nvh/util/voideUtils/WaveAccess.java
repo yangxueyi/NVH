6117b3bc7966251961c30846ef29b1b3c92f3eed
DiYi <646747453@qq.com>
2018-01-10 14:00:31 +0800

    读取wav音频文件的数据

---
+++ app/src/main/java/com/example/zhang/nvh/util/voideUtils/WaveAccess.java
@@ -0,0 +1,9 @@
+package com.example.zhang.nvh.util.voideUtils;
+
+/**
+ * 波形文件(.wav)的读写操作，支持16bit和8bitPCM编码的单双声道文件操作
+ * 
+ */
+public class WaveAccess {
+	public static final String VERSION = "1.1";
+}
